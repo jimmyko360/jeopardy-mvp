81af920b8274a0080a575850e15dc5c108805ca2
Jimmy Ko <ko.jimmy09@gmail.com>
2021-02-09 17:18:48 -0500

    built out show/hide handlers for question component

--- client/components/Question.jsx
+++ client/components/Question.jsx
@@ -4,19 +4,47 @@ class Question extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      displayQuestion: false
+      displayClue: true,
+      displayQuestion: false,
+      displayAnswer: false
     }
     this.showQuestion = this.showQuestion.bind(this);
+    this.showAnswer = this.showAnswer.bind(this);
+    this.hideClue = this.hideClue.bind(this);
+  }
+
+  hideClue() {
+    this.setState({displayClue: !this.state.displayClue})
   }
 
   showQuestion() {
     this.setState({displayQuestion: !this.state.displayQuestion})
   }
 
+  showAnswer() {
+    this.setState({displayAnswer: !this.state.displayAnswer})
+  }
+
   render() {
       return (
-        <div onClick={this.showQuestion}>
-          {this.state.displayQuestion ? this.props.clue.question : this.props.clue.value}
+        <div>
+          {this.state.displayClue ?
+            <div>
+            <div onClick={this.showQuestion}>
+              {this.state.displayQuestion ?
+                this.props.clue.question
+                : this.props.clue.value}
+            </div>
+
+            <div>
+              {this.state.displayAnswer ?
+                this.props.clue.answer
+              : null  }
+            </div>
+            <button onClick={this.showAnswer}>Show Answer</button>
+          </div>
+          : null}
+          <button onClick={this.hideClue}>X</button>
         </div>
     )
   }
