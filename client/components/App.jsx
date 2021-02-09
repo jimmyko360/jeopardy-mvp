83a5732a0dbaa5a83c22e4450c3bf74bc2af0d8a
Jimmy Ko <ko.jimmy09@gmail.com>
2021-02-09 13:54:07 -0500

    requested question categories from API

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,61 @@
 import React from 'react';
+import Category from './Category.jsx'
+import axios from 'axios';
 
 class App extends React.Component {
   constructor() {
     super()
+    this.state = {
+      categoryIds: [],
+      categoryInfo: {}
+    }
+  }
+
+  getCategoryIds() {
+    axios.get('http://jservice.io/api/categories?count=5')
+    .then((categories) => {
+      let idArr = categories.data.map((category) => {
+        return category.id
+      })
+      this.setState({categoryIds: idArr})
+      return idArr
+    })
+    .then((ids) => {
+      let categoryInfo = ids.map((categoryId) => {
+        console.log(categoryId)
+        return axios.get(`http://jservice.io/api/category?id=${categoryId}`)
+      })
+      Promise.all(categoryInfo)
+      .then((categoryPromiseArray) => {
+        let categories = categoryPromiseArray.map((category) => {
+          return category.data
+        })
+        this.setState({categoryInfo: categories})
+      })
+      .catch((err) => {throw(err)})
+    })
+    .catch((err)=>{throw(err)})
+  }
+
+  async getCategoryInfo() {
+    let categoryPromises = this.state.categoryIds.map((categoryId) => {
+      return axios.get(`http://jservice.io/api/category?id=${categoryId}`)
+    })
+    let resolvedCategories = await Promise.all(categoryPromises);
+    this.setState({categoryInfo: resolvedCategories})
+  }
+
+
+  componentDidMount() {
+    this.getCategoryIds();
+    // this.getCategoryInfo();
   }
 
   render() {
     return (
       <div>
         Hello World its me
+        <Category/>
       </div>
     )
   }
