da6a0bde793106a0c7be875dfe369d096d232328
Jimmy Ko <ko.jimmy09@gmail.com>
2021-02-11 11:10:34 -0500

    created server file to served index.html

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
-// let express = require('express');
-// let app = express();
-// app.use(express.json());
+const express = require('express');
+const path = require('path');
+const app = express();
 
-// app.get()
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
